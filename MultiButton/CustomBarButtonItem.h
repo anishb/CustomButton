2f2ee8b028a2adbc7963a2fce7d641122ca39f59
Anish Basu <abasu1983@gmail.com>
2014-03-17 23:55:18 -0700

    Added gitignore file

---
+++ MultiButton/CustomBarButtonItem.h
@@ -0,0 +1,13 @@
+//
+//  CustomBarButtonItem.h
+//  MultiButton
+//
+//  Created by Anish Basu on 3/17/14.
+//  Copyright (c) 2014 Anish Basu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomBarButtonItem : UIBarButtonItem
+
+@end
---
+++ MultiButton/CustomBarButtonItem.m
@@ -0,0 +1,13 @@
+//
+//  CustomBarButtonItem.m
+//  MultiButton
+//
+//  Created by Anish Basu on 3/17/14.
+//  Copyright (c) 2014 Anish Basu. All rights reserved.
+//
+
+#import "CustomBarButtonItem.h"
+
+@implementation CustomBarButtonItem
+
+@end
